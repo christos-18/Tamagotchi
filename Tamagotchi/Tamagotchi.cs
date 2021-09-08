f3bb7c678cd585e0e7c495b1c4bbc81df78eb318
christos <potamitischristos@gmail.com>
2021-09-08 14:58:51 +0200

    1

--- Tamagotchi/Tamagotchi.cs
+++ Tamagotchi/Tamagotchi.cs
@@ -1,11 +1,35 @@
 using System;
+using System.Collections.Generic;
 
-namespace .
+namespace Tamagotchi
 {
     public class Tamagotchi
     {
         private int hunger;
         private int boredom;
-     
-     
-}
+
+        private List<string> words = new List<string>();
+
+        bool isalive = true;
+
+        private Random generator = new Random();
+
+        public string name;
+
+        public static void Feed ();
+        {
+             System.Console.WriteLine("hej");
+            
+         }
+        
+
+
+
+
+
+
+
+
+
+
+    }
